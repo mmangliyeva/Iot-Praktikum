8ddc09f8dc8fa7b36429c631a3d34c04c49a2d1b
Your Name <ge85pip@tum.de>
2023-05-13 14:23:42 +0200

    finally working ISR. able to detect correctly walking in

--- lcd_and_counter/main/lcd_and_counter.c
+++ lcd_and_counter/main/lcd_and_counter.c
@@ -11,13 +11,13 @@
 #define BUFF_STRING_COUNT 4 //size of the buffer to display the number
 #define PIN_DETECT_1 4
 #define PIN_DETECT_2 2
-#define THRESHOLD_DEBOUCE 1000
+#define THRESHOLD_DEBOUCE 50000
 #define PRIO_ANALIZER 5
 #define PRIO_SHOW_COUNT 1
 
 typedef struct Barrier_data
 {
-	uint8_t id; // either 1 or 2
+	uint8_t id; // is the pin 
 	uint8_t state; // 0 NO obsical, 1 there is an obsical
 	int64_t time;
 } Barrier_data;
@@ -34,12 +34,16 @@ TaskHandle_t xShowStateProc = NULL;
 volatile uint8_t count = 0;
 
 //for debounce detector:
-int64_t lastTime_bar = 0; 
-uint8_t lastState_bar = 0;
+int64_t lastTime1 = 0;
+int64_t lastTime2 = 0;
+uint8_t lastState1 = 0;
+uint8_t lastState2 = 0;
+
 
 // function declartion
 void analyzer(void* args);
-void IRAM_ATTR isr_barrier(void* args);
+void IRAM_ATTR isr_barrier1(void* args);
+void IRAM_ATTR isr_barrier2(void* args);
 void showRoomState(void);
  
 
@@ -74,12 +78,13 @@ void app_main(void){
 		// exit(9);
     }
 	
-	gpio_set_intr_type(PIN_DETECT_1, GPIO_INTR_ANYEDGE);
-	gpio_set_intr_type(PIN_DETECT_2, GPIO_INTR_ANYEDGE);
+	gpio_set_intr_type(PIN_DETECT_1, GPIO_INTR_POSEDGE);
+	gpio_set_intr_type(PIN_DETECT_2, GPIO_INTR_POSEDGE);
 	// init isr
 	gpio_install_isr_service(0);
-    gpio_isr_handler_add(PIN_DETECT_1, isr_barrier, (void*)PIN_DETECT_1);
-    gpio_isr_handler_add(PIN_DETECT_2, isr_barrier, (void*)PIN_DETECT_2);
+	//(void*)PIN_DETECT_1
+    gpio_isr_handler_add(PIN_DETECT_1, isr_barrier1, NULL);
+    gpio_isr_handler_add(PIN_DETECT_2, isr_barrier2, NULL);
 
 	// start task, for analyzing the 
 	xTaskCreate(analyzer, "analizer", 4096, NULL, PRIO_ANALIZER, NULL);
@@ -96,11 +101,10 @@ void analyzer(void *args){
 	while(1){
 		if( xQueueReceive(queue, &data, (TickType_t)5) ){
 			// sprintf(outputMessage,"id: %d state: %d time %ld", data.id, data.state, (long)data.time);
-			
 			ESP_LOGI("MY_ANALIZER", "id: %d state: %d time %ld", data.id, data.state, (long)data.time);
 			count++;
 		}
-		vTaskDelay(500 / portTICK_PERIOD_MS);
+		vTaskDelay(100 / portTICK_PERIOD_MS);
 	}
 	
 							
@@ -112,22 +116,37 @@ void showRoomState(void){
 	char str[BUFF_STRING_COUNT];
 	//converts int to string
 	sprintf(str,"%d",count);
-	ssd1306_printFixedN(0,16,str,STYLE_BOLD,2);	
+	ssd1306_printFixedN(0,16,str,STYLE_BOLD,2);
 }
 
 
 
-// here is something incorrect
-void IRAM_ATTR isr_barrier(void* args){
-	int64_t curTime = esp_timer_get_time();
-	uint8_t curState = gpio_get_level((uint8_t)args);
-	//debounce code
-	if((lastState_bar != curState)
-		&& (curTime - lastTime_bar > THRESHOLD_DEBOUCE)){
-		Barrier_data data = {(uint8_t)args,curState,curTime};
-		xQueueSendFromISR(queue,&data,(TickType_t)0);
-	}
-	lastState_bar = curState;
-	lastTime_bar = curTime;
+// for detector PIN_DETECT_1
+void IRAM_ATTR isr_barrier1(void* args){
+	uint8_t curState = gpio_get_level(PIN_DETECT_2);
+	// //debounce code
+	if(lastState1 != curState){
+		int64_t curTime = esp_timer_get_time();
+		if (curTime - lastTime1 > THRESHOLD_DEBOUCE){
+			// AND last statechange long ago
+			Barrier_data data = {1,curState,curTime};
+			xQueueSendFromISR(queue,&data,(TickType_t)0);
+			lastState1 = curState;
+		}
+		lastTime1 = curTime;
+	}	
 }
-
+void IRAM_ATTR isr_barrier2(void* args){
+	uint8_t curState = gpio_get_level(PIN_DETECT_2);
+	// //debounce code
+	if(lastState2 != curState){
+		int64_t curTime = esp_timer_get_time();
+		if (curTime - lastTime2 > THRESHOLD_DEBOUCE){
+			// AND last statechange long ago
+			Barrier_data data = {2,curState,curTime};
+			xQueueSendFromISR(queue,&data,(TickType_t)0);
+			lastState2 = curState;
+		}
+		lastTime2 = curTime;
+	}	
+}
